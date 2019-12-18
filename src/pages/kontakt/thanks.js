49cdd2ad7538d0754f41903f843d030b315d174e
Jakub Wojtyra <wojtyraj@gmail.com>
2019-12-18 18:13:50 +0100

    Thanks page

--- src/pages/kontakt/thanks.js
+++ src/pages/kontakt/thanks.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import Logo from '../../img/logo.png'
 
 export default () => (
   <Layout>
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <img className="hoverable" src={Logo} width="210" />
+          <p>Twoja wiadomość została wysłana. Dziękuję za zainteresowanie moją hodowlą!</p>
         </div>
       </div>
     </section>
